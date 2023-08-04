8a90e84edcf7b220706617229e45f4b36e760b17
Jonathan <jon.bourquin@gmail.com>
2023-08-04 21:57:19 +0200

    custom marker icons added

--- lib/museum/museum_marker.dart
+++ lib/museum/museum_marker.dart
@@ -3,10 +3,16 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'museum.dart';
 import 'museum_info_popup.dart';
 
-Marker createMuseumMarker(BuildContext context, Museum museum) {
+Future<Marker> createMuseumMarker(BuildContext context, Museum museum) async {
+  BitmapDescriptor markerbitmap = await BitmapDescriptor.fromAssetImage(
+    ImageConfiguration(devicePixelRatio: 10),
+    "assets/musee.png",
+  );
+
   return Marker(
     markerId: MarkerId(museum.name),
     position: museum.location,
+    icon: markerbitmap,
     onTap: () {
       print(museum.name);
       
@@ -18,3 +24,4 @@ Marker createMuseumMarker(BuildContext context, Museum museum) {
     },
   );
 }
+
