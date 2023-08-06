73f9f7b584a990aa5432cd94e818938540062df6
Jonathan <jon.bourquin@gmail.com>
2023-08-06 19:14:20 +0200

    main commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:exponomade/appNavigation/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:exponomade/database/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
