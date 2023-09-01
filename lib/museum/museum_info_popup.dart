3c23b3350b9638f1b8d09c1378db30306d0e2acc
JeremDel <jeremie.dellea@hotmail.com>
2023-09-01 19:00:01 +0200

    modification pop-up musée

--- lib/museum/museum_info_popup.dart
+++ lib/museum/museum_info_popup.dart
@@ -4,38 +4,131 @@ import 'package:flutter/material.dart';
 class MuseumInfoPopup extends StatelessWidget {
   final Musee musee;
 
-  const MuseumInfoPopup({super.key, required this.musee});
+  const MuseumInfoPopup({Key? key, required this.musee}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text(musee.nomMusee),
+      titlePadding: EdgeInsets.all(16.0),
+      title: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.blueGrey[300]!, width: 2),
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Musée :',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[600],
+                    fontSize: 16.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  musee.nomMusee,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[600],
+                    fontSize: 18.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      contentPadding: EdgeInsets.all(20.0),
       content: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: musee.objets.map((objet) {
-            return ListTile(
-              title: Text("Nom: ${objet.nomObjet}"),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.network(
-                    objet.image,
-                    errorBuilder: (BuildContext context, Object exception,
-                        StackTrace? stackTrace) {
-                      // Affiche un message d'erreur ou une image de remplacement en cas d'erreur.
-                      return const Text(
-                          'Erreur lors du chargement de l\'image');
-                    },
-                    width: 300,
+            return Column(
+              children: <Widget>[
+                Divider(
+                  color: Colors.blueGrey[300],
+                  thickness: 2.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  child: ListTile(
+                    title: Text(
+                      "Objet: ${objet.nomObjet}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.blueGrey[600],
+                        fontSize: 16.0,
+                      ),
+                    ),
                   ),
-                  Text("Description: ${objet.descriptionObjet}"),
-                  Text(
-                      "De ${objet.chronologie['from']} à ${objet.chronologie['to']}"),
-                  Text("Raison de la migration: ${objet.raisons.join(", ")}"),
-                  Text("Populations: ${objet.population}"),
-                ],
-              ),
+                ),
+                Divider(
+                  color: Colors.blueGrey[300],
+                  thickness: 2.0,
+                ),
+                ListTile(
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.network(
+                        objet.image,
+                          errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                              return Text(
+                                           'Erreur lors du chargement de l\'image',
+                           style: TextStyle(color: Colors.red, fontStyle: FontStyle.italic),
+                         );
+                        },
+                   width: 300,
+                ),
+
+                      SizedBox(height: 6.0),
+                      Text(
+                        "Description:",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 6.0), // Retour à la ligne supplémentaire
+                      Text(objet.descriptionObjet),
+                      SizedBox(height: 6.0),
+                      Row(
+                        children: [
+                          Text(
+                            "De ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text("${objet.chronologie['from']}"),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "à ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text("${objet.chronologie['to']}"),
+                        ],
+                      ),
+                      SizedBox(height: 6.0),
+                      Text(
+                        "Raison de la migration:",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(objet.raisons.join(", ")),
+                      SizedBox(height: 6.0),
+                      Text(
+                        "Populations:",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(objet.population),
+                    ],
+                  ),
+                ),
+              ],
             );
           }).toList(),
         ),
