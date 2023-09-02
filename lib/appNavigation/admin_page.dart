9353949e11f691cdd79ea4bb45f34395f1b2fe24
Bastien Salamin <bastien.salamin@hes-so.ch>
2023-09-02 22:51:36 +0200

    Edition de musée sur la même page d'admin

--- lib/appNavigation/admin_page.dart
+++ lib/appNavigation/admin_page.dart
@@ -3,7 +3,7 @@ import '../utils/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../widgets/custom_textfield.dart';
 import '../utils/validators.dart';
-import 'admin_console_page.dart';
+import '../zones/zoneAdminPage.dart';
 
 class AdminPage extends StatefulWidget {
   @override
@@ -13,9 +13,18 @@ class AdminPage extends StatefulWidget {
 class _AdminPageState extends State<AdminPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
+  List<String> collections = ['Musées', 'Zones'];
+  bool showMuseumAdminPage = false;
+  bool showZoneAdminPage = false;
   User? _user; // To store the authenticated user
   String? _signInError;
+
+// For the sake of demonstration, I'll use a static list, but you can fetch it from Firestore.
+  List<String> museums = [
+    'Musée de Bagnes',
+    'Another Museum',
+    'And another one'
+  ];
 
   @override
   void initState() {
@@ -26,10 +35,9 @@ class _AdminPageState extends State<AdminPage> {
   // Function to check if a user is already authenticated
   Future<void> _checkAuthenticationStatus() async {
     User? user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => AdminConsolePage()));
-    }
+    setState(() {
+      _user = user;
+    });
   }
 
   // Function to sign in with email and password
@@ -46,11 +54,6 @@ class _AdminPageState extends State<AdminPage> {
         _user = userCredential.user;
         _signInError = null;
       });
-
-      if (_user != null) {
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => AdminConsolePage()));
-      }
 
       // Successful sign-in, you can now perform admin actions.
       // Access the signed-in user: userCredential.user
@@ -84,16 +87,24 @@ class _AdminPageState extends State<AdminPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: background,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Administration'),
-        backgroundColor: background,
-      ),
+      appBar: _user != null
+          ? AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Administration'),
+              backgroundColor: background,
+              actions: [
+                  IconButton(icon: Icon(Icons.logout), onPressed: _signOut),
+                ])
+          : AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Administration'),
+              backgroundColor: background,
+            ),
       body: Center(
         child: _user == null
             ? _buildSignInForm() // Show sign-in form when not authenticated
-            : _buildAdminActions(), // Show admin actions when authenticated,
-      ),
+            : _buildAdminActions(),
+      ), // Show admin actions when authenticated,
     );
   }
 
@@ -171,43 +182,119 @@ class _AdminPageState extends State<AdminPage> {
   Widget _buildAdminActions() {
     return Padding(
       padding: const EdgeInsets.fromLTRB(25.0, 40, 25, 0),
-      child: Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 600),
-          child: Column(
-            children: [
-              Text(
-                "Bienvenue ${_user!.email} !",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(height: 25),
-              // Insérer un bouton ou le formulaire de création de zones ici
-              SizedBox(height: 25),
-              ElevatedButton(
-                onPressed: _signOut,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: neutral,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                ),
-                child: const Text(
-                  "Se déconnecter",
+      child: showMuseumAdminPage
+          ? _buildMuseumAdminPage()
+          : Column(
+              children: [
+                Text(
+                  "Bienvenue ${_user!.email} !",
                   style: TextStyle(
                     fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
+                SizedBox(height: 25),
+                Text(
+                  "Modifier la base de données :",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: collections.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                          collections[index],
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onTap: () {
+                          if (index == 0) {
+                            setState(() {
+                              showMuseumAdminPage =
+                                  true; // Show MuseumAdminPage
+                            });
+                          } else if (index == 1) {
+                            // index for zones
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ZoneAdminPage(),
+                              ),
+                            );
+                          }
+                        },
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+
+  Widget _buildMuseumAdminPage() {
+    return Scaffold(
+      backgroundColor: background,
+      body: Column(
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: neutral,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
               ),
-            ],
+            ),
+            onPressed: () {
+              setState(() {
+                showMuseumAdminPage = false; // Hide the MuseumAdminPage
+              });
+            },
+            child: Text("Retour",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                )),
           ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: museums.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(museums[index],
+                      style: TextStyle(
+                        color: Colors.white,
+                      )),
+                  trailing: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    // Handle tap to edit museum details or other admin actions.
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: neutral,
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
         ),
+        onPressed: () {
+          // Add a new museum.
+        },
       ),
     );
   }
