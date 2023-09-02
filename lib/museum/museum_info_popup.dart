ad7e5fc255d6736fb205fae6aaf0ffa1fda2caf9
Theoc <theo.clerc@students.hevs.ch>
2023-09-02 15:09:58 +0200

    Resize pop-up

--- lib/museum/museum_info_popup.dart
+++ lib/museum/museum_info_popup.dart
@@ -8,147 +8,151 @@ class MuseumInfoPopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      titlePadding: EdgeInsets.all(16.0),
-      title: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.blueGrey[300]!, width: 2),
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-            child: Column(
-              children: [
-                Text(
-                  'Musée :',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue[600],
-                    fontSize: 16.0,
-                  ),
-                  textAlign: TextAlign.center,
+    return Center(
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 600), // Ajustez la largeur maximale selon vos besoins
+        child: AlertDialog(
+          titlePadding: EdgeInsets.all(16.0),
+          title: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueGrey[300]!, width: 2),
+                  borderRadius: BorderRadius.circular(12.0),
                 ),
-                SizedBox(height: 8.0),
-                Text(
-                  musee.nomMusee,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue[600],
-                    fontSize: 18.0,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      contentPadding: EdgeInsets.all(20.0),
-      content: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: musee.objets.map((objet) {
-            return Column(
-              children: <Widget>[
-                Divider(
-                  color: Colors.blueGrey[300],
-                  thickness: 2.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: ListTile(
-                    title: Text(
-                      "Objet: ${objet.nomObjet}",
-                      textAlign: TextAlign.center,
+                child: Column(
+                  children: [
+                    Text(
+                      'Musée :',
                       style: TextStyle(
-                        color: Colors.blueGrey[600],
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[600],
                         fontSize: 16.0,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                  ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      musee.nomMusee,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[600],
+                        fontSize: 18.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
-                Divider(
-                  color: Colors.blueGrey[300],
-                  thickness: 2.0,
-                ),
-                ListTile(
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Image.network(
-                        objet.image,
-                          errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+              ),
+            ],
+          ),
+          contentPadding: EdgeInsets.all(20.0),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: musee.objets.map((objet) {
+                return Column(
+                  children: <Widget>[
+                    Divider(
+                      color: Colors.blueGrey[300],
+                      thickness: 2.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: ListTile(
+                        title: Text(
+                          "Objet: ${objet.nomObjet}",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.blueGrey[600],
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.blueGrey[300],
+                      thickness: 2.0,
+                    ),
+                    ListTile(
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Image.network(
+                            objet.image,
+                            errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
                               return Text(
-                                           'Erreur lors du chargement de l\'image',
-                           style: TextStyle(color: Colors.red, fontStyle: FontStyle.italic),
-                         );
-                        },
-                   width: 300,
-                ),
-
-                      SizedBox(height: 6.0),
-                      Text(
-                        "Description:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 6.0), // Retour à la ligne supplémentaire
-                      Text(objet.descriptionObjet),
-                      SizedBox(height: 6.0),
-                      Row(
-                        children: [
+                                'Erreur lors du chargement de l\'image',
+                                style: TextStyle(color: Colors.red, fontStyle: FontStyle.italic),
+                              );
+                            },
+                            width: 300,
+                          ),
+                          SizedBox(height: 6.0),
                           Text(
-                            "De ",
+                            "Description:",
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                          Text("${objet.chronologie['from']}"),
-                        ],
-                      ),
-                      Row(
-                        children: [
+                          SizedBox(height: 6.0), // Retour à la ligne supplémentaire
+                          Text(objet.descriptionObjet),
+                          SizedBox(height: 6.0),
+                          Row(
+                            children: [
+                              Text(
+                                "De ",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text("${objet.chronologie['from']}"),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "à ",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text("${objet.chronologie['to']}"),
+                            ],
+                          ),
+                          SizedBox(height: 6.0),
                           Text(
-                            "à ",
+                            "Raison de la migration:",
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                          Text("${objet.chronologie['to']}"),
+                          Text(objet.raisons.join(", ")),
+                          SizedBox(height: 6.0),
+                          Text(
+                            "Populations:",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text(objet.population),
                         ],
                       ),
-                      SizedBox(height: 6.0),
-                      Text(
-                        "Raison de la migration:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(objet.raisons.join(", ")),
-                      SizedBox(height: 6.0),
-                      Text(
-                        "Populations:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(objet.population),
-                    ],
-                  ),
-                ),
-              ],
-            );
-          }).toList(),
-        ),
-      ),
-      actions: <Widget>[
-        TextButton(
-          style: TextButton.styleFrom(
-            primary: Colors.white,
-            backgroundColor: Colors.blue[400],
-            onSurface: Colors.grey,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                  ],
+                );
+              }).toList(),
             ),
           ),
-          child: const Text('Fermer'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.blue[400],
+                onSurface: Colors.grey,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+              ),
+              child: const Text('Fermer'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
